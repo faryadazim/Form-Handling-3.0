3d91835e182840b985921043fe10fa1e88cecc58
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-06 15:03:04 +0500

    update

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
+const port = process.env.port || 3000;
 var bodyParser = require("body-parser");
-const port = process.env.port || 3001;
-var dbConnection = require("./src/config/db");
-dbConnection();
+var cors = require("cors");
 
-// MiddleWare
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// __________MidcdleWare
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 
-// Rendering
-
+// __________Home Page
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+  res.send("Hello World!");
 });
 
-// Routes
-var routes = require("./src/user/Routes");
+// __________Routes
+const routes = require("./src/user/Routes");
 app.use("/routes", routes);
 
 app.listen(port, () => {
-  try {
-    console.log(`CRUD App listening at http://localhost:${port}`);
-  } catch (error) {
-    console.log("Error :", error);
-  }
+  console.log(`Backend app listening at http://localhost:${port}`);
 });
--- src/user/Routes.js
+++ src/user/Routes.js
@@ -1,17 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
-// const addData = require("./Controller");
-// middleware that is specific to this router
-
 // define the home page route
 router.get("/", function (req, res) {
   res.send("Route home page");
 });
 // define the about route
-router.post("/user", function (req, res) {
-  res.send("About route");
-  // addData();
+router.get("/about", function (req, res) {
+  res.send("About Routes");
 });
 
 module.exports = router;
